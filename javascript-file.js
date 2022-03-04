57d5f9fd30ae9691d31ce947814a4cce76bbc3d0
gwill-01 <gjwill14@gmail.com>
2022-03-04 04:43:00 -0800

    Add computer play function

---
+++ javascript-file.js
@@ -0,0 +1,14 @@
+/* computerPlay function */
+
+let computerArray = ['rock', 'paper', 'scissors']
+
+function computerPlay() {
+    return computerArray[Math.floor(Math.random()*computerArray.length)];
+} 
+
+/* rock paper scissors single round function */
+    /* player enters case INsensitive selection */
+    /* runs player selection vs computer function */
+    /* returns text of round winner */
+
+/* game function keeps track of 5 rounds */
