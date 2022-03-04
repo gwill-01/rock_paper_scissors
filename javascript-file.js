34dd99c0887cb3e8857c7fc65b9322cdd5488b49
gwill-01 <gjwill14@gmail.com>
2022-03-04 06:16:08 -0800

    Add lowercase entry

--- javascript-file.js
+++ javascript-file.js
@@ -9,31 +9,33 @@ function computerPlay() {
 /* rock paper scissors single round function */
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return "You Win";
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return "You Lose";
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return "You Win";
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return "You Lose";
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return "You Win";
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return "You Lose";
-    } else if (playerSelection == computerSelection) {
-        return "Its a wash";
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+        return "You win this round";
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+        return "You lose this round";
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        return "You win this round";
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        return "You lose this round";
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        return "You win this round";
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        return "You win this round";
+    } else if (playerSelection.toLowerCase() == computerSelection) {
+        return "This round is a wash";
     } else {
         return "Do you not know the rules?";
     }
 }
-/* player enters case INsensitive selection */
 
-const playerSelection = 'paper';
+const playerSelection = 'PAPER';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
     
-/* runs player selection vs computer function */
-    /* returns text of round winner */
-
 /* game function keeps track of 5 rounds */
+
+function game() {
+    
+}
+
+/* Allow custom player selection */
